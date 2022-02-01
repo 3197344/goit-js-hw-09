ebb2a3586298f364d4643cad4ea0fa098320d24c
Nadia Lut <3197344@gmail.com>
2022-02-01 20:56:45 +0200

    done task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const promptDeley = 1000;
+let intervalID = null;
+
+btnStart.addEventListener('click', changesBackgroundColorOfBody );
+btnStop.addEventListener('click', stopChangesBackgroundColorOfBody);
+
+
+function changesBackgroundColorOfBody() {
+  //  event.preventDefault();
+    intervalID = setInterval(() => {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    console.log('генерим цвет', `${getRandomHexColor()}`);
+    }, 1000);
+
+}
+
+function stopChangesBackgroundColorOfBody() {
+    clearInterval(intervalID);
+console.log('хватит', btnStop); 
+}
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 
