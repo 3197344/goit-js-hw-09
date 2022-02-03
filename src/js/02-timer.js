5090824a31daa1c0b06da2bf3f68070f16b10e45
Nadia Lut <3197344@gmail.com>
2022-02-03 19:29:45 +0200

    finish task 02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,10 +9,26 @@ daysSpan: document.querySelector('.value[data-days]'),
 hoursSpan: document.querySelector('.value[data-hours]'),
 minutesSpan: document.querySelector('.value[data-minutes]'),
 secondsSpan: document.querySelector('.value[data-seconds]'),
+timerSpan:document.querySelector('.timer'),
+fieldSpan:document.querySelectorAll('.field'),
+numberSpan: document.querySelectorAll('.value'),
+labelSpan: document.querySelectorAll('.label'),
 }
+refs.btnStart.disabled = true;
+refs.btnStart.style.width = "80px"
+refs.btnStart.style.height = "30px"
+refs.btnStart.style.margin = '20px'
+refs.timerSpan.style.display ="flex";
+refs.timerSpan.style.width = "100px"
+
+refs.timerSpan.style.textAlign = "center"
+refs.hoursSpan.style.margin = '30px'
+refs.daysSpan.style.margin = '30px'
+refs.minutesSpan.style.margin = '30px'
+refs.secondsSpan.style.margin = '30px'
+
 
 refs.btnStart.setAttribute('disabled', 'true');
-
 
 
 const options = {
@@ -21,59 +37,102 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
+        const inputDate = selectedDates[0];
         const currentTime = Date.now();
-        console.log(selectedDate);
-
-        if (currentTime > selectedDate) {
-        Notiflix.Notify.failure('please, choose a date in the future');
+        const deltaTime = (inputDate - currentTime);
+        if (deltaTime > 0) {
+            refs.btnStart.disabled = false;
+        
+            refs.btnStart.style.backgroundColor = "purple";
+            refs.btnStart.style.color = "white";
+            refs.hoursSpan.style.color = "purple";
+            refs.daysSpan.style.color = "purple";
+            refs.minutesSpan.style.color = "purple";
+            refs.secondsSpan.style.color = "purple";
         }
-        refs.btnStart.disabled = false;
-        refs.btnStart.style.backgroundColor = "purple";
-        refs.btnStart.style.color = "white";
+        else {
+        // window.alert("Please choose a date in the future");
+        Notiflix.Notify.failure('Please choose a date in the future');
+        }
     },
 };
 
 // Инициализируем библиотеку flatpickr*/ 
-const viewCalendar = flatpickr(refs.inputClock, options);
+const fp = flatpickr(refs.inputClock, options);
+// console.log(fp);
 
+// Создаём таймер по видео Репеты
+class Timer {
+    constructor({ onTick }) {
+        this.intervalID = null;
+        this.isActive = false;
+        this.onTick = onTick;
+    }
 
+    start() {
+        if (this.isActive) {
+        return;
+    }
+    const selectedTime = fp.selectedDates[0].getTime();
+    this.isActive = true;
 
+    this.intervalID = setInterval(() => {
+        const currentTime = Date.now();
+        const deltaTime = selectedTime - currentTime;
+        const convertedTime = this.convertMs(deltaTime);
+        this.onTick(convertedTime);
+        if (deltaTime <= 0) {
+            this.stop();
+        }
+    }, 1000);
+    }
 
+    stop() {
+        clearInterval(this.intervalID);
+        this.isActive = false;
+        const convertedTime = this.convertMs(0);
+        this.onTick(convertedTime);
+    }
 
+    convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
+    // Remaining days
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
+    return { days, hours, minutes, seconds };
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function convertMs(ms) {
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-
-const days = Math.floor(ms / day);
-const hours = Math.floor((ms % day) / hour);
-const minutes = Math.floor(((ms % day) % hour) / minute);
-const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-return { days, hours, minutes, seconds };
+    addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+    }
 }
 
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+const timer = new Timer({ onTick: faceClock });
 
-// alert ('please, choose a date in the future')
+refs.btnStart.addEventListener('click', onClickTimerStart);
 
+// Функция для кнопки СТАРТ
+
+function onClickTimerStart() {
+  timer.start();
+}
+
+// Функция для отрисовки оставшегося времени на странице
+
+function faceClock(value) {
+refs.daysSpan.innerHTML = value.days;
+refs.hoursSpan.innerHTML = value.hours;
+refs.minutesSpan.innerHTML = value.minutes;
+refs.secondsSpan.innerHTML = value.seconds;
+}
